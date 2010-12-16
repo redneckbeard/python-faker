0e1cb321c6a6095ab52d8dda6bd115ad4ae60956
Denis Redozubov <denis.redozubov@gmail.com>
2010-12-16 12:46:20 +0300

    numerify will return only one-digit numbers

--- src/faker/generators/utils.py
+++ src/faker/generators/utils.py
@@ -6,7 +6,7 @@ numerify_re = re.compile('#')
 letterify_re = re.compile(r'\?')
 
 def numerify(number_string):
-    return numerify_re.sub(lambda m: str(random.randint(0, 10)), number_string)
+    return numerify_re.sub(lambda m: str(random.randint(0, 9)), number_string)
 
 def letterify(letter_string):
     return numerify_re.sub(lambda m: random.choice(string.ascii_letters), number_string)
